f432843f3dbcf1ff05b35b16afffde96d309627f
ANDRÉS ALEJANDRO LÓPEZ TORRES <andres.lopez@mail.udp.cl>
2021-12-01 15:43:41 -0300

    modifacion Tabla carrito contenido

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -19,7 +19,7 @@ export function CartContents() {
             <h1> Tu carrito </h1>
             {!!checkout?.lineItems && (
             <CartHeader>
-                <div>Productos</div>
+                <div>Productosss</div>
                 <div>Precio Unitario</div>
                 <div>Cantidad</div>
                 <div>Monto </div>
